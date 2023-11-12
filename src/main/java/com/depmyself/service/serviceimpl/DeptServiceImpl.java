e3fb45769eb2b17a83e468baab3d62a3058a3f39
xyxwfg <1510882710@qq.com>
2023-11-12 16:15:10 +0800

    2,在此处复习与修改1112

--- src/main/java/com/depmyself/service/serviceimpl/DeptServiceImpl.java
+++ src/main/java/com/depmyself/service/serviceimpl/DeptServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -21,7 +22,7 @@ import java.util.List;
 @Service
 public class DeptServiceImpl implements DeptService {
 
-    @Autowired
+    @Resource
     private DeptMapper deptMapper;
 
     @Override
